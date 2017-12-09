f02b0301440a8b140b55121ef4be1de75f7de64a
cash <cash@cashwu.com>
2017-12-09 11:18:01 +0800

    unit test2

--- AutoFixtureSample/UnitTest2.cs
+++ AutoFixtureSample/UnitTest2.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ExpectedObjects;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+using System.Linq;
 
 namespace AutoFixtureSample
 {
@@ -10,6 +13,10 @@ namespace AutoFixtureSample
         public void TestMethod1()
         {
             // 使用 AutoFixture 建立 MyClass 類別的物件
+            var fixture = new Fixture();
+            var actual = fixture.Create<MyClass>();
+            actual.Should().NotBeNull();
+            actual.Should().BeOfType<MyClass>();
         }
 
         [TestMethod]
@@ -18,6 +25,14 @@ namespace AutoFixtureSample
             // 使用 AutoFixture 建立 MyClass 類別的物件
             // 設定 Inject 給予預設值
             // 使用 Create 方法
+
+            var fixture = new Fixture();
+            fixture.Inject("cash");
+
+            var actual = fixture.Create<MyClass>();
+
+            actual.Name.Should().StartWith("cash");
+            actual.Orders.Select(x => x.Name).All(x => x.StartsWith("cash")).Should().BeTrue();
         }
 
         [TestMethod]
@@ -25,6 +40,18 @@ namespace AutoFixtureSample
         {
             // 使用 AutoFixture 建立 MyClass 類別的物件
             // 使用 Build 方法以及 OmitAutoProperties
+            var expected = new MyClass();
+
+            var fixture = new Fixture();
+
+            var actual = fixture.Build<MyClass>()
+                                .OmitAutoProperties()
+                                .Create();
+
+            actual.Should().NotBeNull();
+            expected.ToExpectedObject().ShouldEqual(actual);
+
+            actual.ShouldBeEquivalentTo(expected);
         }
 
         [TestMethod]
@@ -32,6 +59,15 @@ namespace AutoFixtureSample
         {
             // 使用 AutoFixture 建立 MyClass 類別的物件
             // 使用 Build 方法以及 Without 和 With
+            var fixture = new Fixture();
+
+            var actual = fixture.Build<MyClass>()
+                                .With(x => x.Name, "cash")
+                                .Without(x => x.Age)
+                                .Create();
+
+            actual.Name.Should().Be("cash");
+            actual.Age.Should().Be(0);
         }
 
         [TestMethod]
@@ -39,6 +75,11 @@ namespace AutoFixtureSample
         {
             // 使用 AutoFixture 建立 MyClass 類別的物件
             // 使用 CreateMany 方法，讓物件集合裡有 5 個物件
+            var fixture = new Fixture();
+
+            var actual = fixture.CreateMany<MyClass>(5);
+
+            actual.Count().Should().Be(5);
         }
 
         [TestMethod]
@@ -46,6 +87,20 @@ namespace AutoFixtureSample
         {
             // 使用 AutoFixture 建立 MyClass 類別的物件
             // 使用 With 方法，讓物件的屬性 Orders 有 3 個 order 物件
+            var fixture = new Fixture();
+
+            //var orders = fixture.CreateMany<Order>(3);
+
+            //var actual = fixture.Build<MyClass>()
+            //                    .With(x => x.Orders, orders.ToList())
+            //                    .Create();
+
+            var actual = fixture.Build<MyClass>()
+                                .With(x => x.Orders, fixture.CreateMany<Order>(3).ToList())
+                                .Create();
+
+            actual.Orders.Any().Should().BeTrue();
+            actual.Orders.Count.Should().Be(3);
         }
 
         [TestMethod]
@@ -53,6 +108,14 @@ namespace AutoFixtureSample
         {
             // 使用 AutoFixture 建立 MyClass 類別的物件
             // 使用 Do 方法，讓物件的屬性 Orders 有 3 個 Order 物件
+            var fixture = new Fixture();
+
+            var actual = fixture.Build<MyClass>()
+                                .Do(x => x.Orders.AddRange(fixture.CreateMany<Order>(3)))
+                                .Create();
+
+            actual.Orders.Any().Should().BeTrue();
+            actual.Orders.Count.Should().Be(3);
         }
     }
 }
